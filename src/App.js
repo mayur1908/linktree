9ab457c3c3b8348feeeb05203c0160432ec17255
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2022-06-18 13:29:42 +0530

    third-commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,38 @@ import './App.css';
 import LinkCard from './LinkCard/LinkCard';
 
 function App() {
+  const data = [
+    {
+      title: 'Twitter',
+      tagline: "follow me on twitter",
+      username: '@MayurSonkusare9'
+
+    },
+    {
+      title: 'Linkdin',
+      tagline: "follow me on Linkdin",
+      username: '@MayurSonkusare9'
+
+    },
+    {
+      title: 'Git-Hub',
+      tagline: "follow me on Git-Hub",
+      username: 'mayur1908'
+
+    }
+  ]
   return (
     <div className="Container">
       <h1 className='app-title'>Linktree-Alt</h1>
-      <LinkCard/>
+      {
+        data.map((cardinfo) => {
+          return <LinkCard title={cardinfo.title}
+            tagline={cardinfo.tagline}
+            username={cardinfo.username}
+          />
+        }
+        )
+      }
     </div>
   );
 }
--- src/LinkCard/LinkCard.js
+++ src/LinkCard/LinkCard.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import './LinkCard.css'
-import imgTwitter from './../img/twitter.jpg'
-function LinkCard() {
+import imgTwitter from './../img/twitter.png'
+function LinkCard(props) {
   return (
     <div className='link-card'>
-    <div className='container-link-handle'>
+      <div className='container-link-handle'>
         <div>
-            <img src={imgTwitter} className='img-card'/>
+          <img src={imgTwitter} className='img-card' />
         </div>
         <div className='handle-name'>
-            <h3>Twitter Handle</h3>
+          <h3>{props.username}</h3>
         </div>
+      </div>
+      <div className='handle-discription'>
+        <p>{props.tagline} </p>
+      </div>
     </div>
-    
-
-   </div> 
   )
 }
 
